ee645207876fb81e1921c29b5040d2da85015a85
baohan2909 <baohan2909@gmail.com>
2026-07-26 09:59:02 +0700

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// base tuyệt đối theo tên repo GitHub Pages (baohan2909.github.io/nscare/)
-export default defineConfig({
-  plugins: [react()],
-  base: '/nscare/',
-  build: { outDir: 'dist', sourcemap: false }
-})
